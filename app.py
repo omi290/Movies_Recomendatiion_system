762d82f54c332930eb2f7e0b46958c70e51288ce
Om prakash barmola <omp723033@gmail.com>
2025-01-13 08:25:51 +0530

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def recommend(movie):
     return recommended_movie_names, recommended_movie_posters
 
 # Streamlit app
-st.header('Movie Recommender System')
+st.header('Movie Recommendation System')
 movies = pickle.load(open('movie_list.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
